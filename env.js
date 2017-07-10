4ad61fc3dca27d1e11e2c07179713b36c8468374
vectorsize <vectorsize@gmail.com>
2017-07-10 15:36:19 +0200

    new build

--- env.js
+++ env.js
@@ -1,7 +1,5 @@
 window.process = {
   env: {
-    // SANDBOX_ENDPOINT: 'http://54.224.10.82/api/v1/stats/all/country'
-    // SANDBOX_ENDPOINT: 'http://localhost:90/api/v1/stats/all/country'
-    SANDBOX_ENDPOINT: './mocks/histogram-features-hti.json'
+    SANDBOX_ENDPOINT: 'mocks/histogram-features-hti.json'
   }
 }
