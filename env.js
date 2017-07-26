fc6b1524ed95d9edb895430abd810e4d62cb7233
vectorsize <vectorsize@gmail.com>
2017-07-26 11:51:21 +0200

    update build

--- env.js
+++ env.js
@@ -1,5 +1,6 @@
 window.process = {
   env: {
-    SANDBOX_ENDPOINT: 'mocks/histogram-features-hti.json'
+    // SANDBOX_ENDPOINT: 'mocks/histogram-features-hti.json?'
+    SANDBOX_ENDPOINT: 'https://osm-analytics.vizzuality.com/api/v1'
   }
 }
